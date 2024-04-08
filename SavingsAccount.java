69ee1c3ff501d40bd324c2842e29730b4b029e12
thienngonguyenle <tle61@msudenver.edu>
2024-04-07 22:24:39 -0600

    update starter code for SavingAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,1 +1,78 @@
 
+public class SavingsAccount
+{
+   private double balance;          // The account balance
+   private double interestRate;     // The annual interest rate
+   private double lastInterest;     // Last amount of interest earned
+
+   /**
+    * The constructor initializes an object with a
+    * balance and an annual interest rate.
+    */
+
+   public SavingsAccount(double bal, double intRate)
+   {
+	   
+   }
+
+   /**
+    * The withdraw method withdraws an amount from
+    * the account.
+    */
+
+   public void withdraw(double amount)
+   {
+      
+   }
+   
+   /**
+    * The deposit method deposits an amount into
+    * the account.
+    */
+
+   public void deposit(double amount)
+   {
+  
+   }
+
+   /**
+    * The addInterest method calculates the monthly
+    * interest and adds it to the account balance.
+    */
+
+   public void addInterest()
+   {
+      
+   }
+
+   /**
+    * The getBalance method returns the account balance.
+    */
+
+   public double getBalance()
+   {
+      return 0.0;
+   }
+
+   /**
+    * The getInterestRate method returns the annual
+    * interest rate.
+    */
+
+   public double getInterestRate()
+   {
+      return 0.0;
+   }
+
+   /**
+    * The getLastInterest method returns the last amount
+    * of interest earned.
+    */
+
+   public double getLastInterest()
+   {
+      return 0.0;
+   }
+
+}
+
